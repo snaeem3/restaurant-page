40bd6325e204323bcc8abbae095f19b4b1ffdac5
Sameer <sameer4363@gmail.com>
2023-01-06 21:52:15 -0500

    Add eslint files

--- src/page-load.js
+++ src/page-load.js
@@ -1,36 +1,36 @@
-import loadHome from "./home";
+import loadHome from './home';
+
 const contentDiv = document.querySelector('#content');
 
 export default function loadPage() {
-    console.log('page-load.js called');
+  console.log('page-load.js called');
 
-    
-    createHeader();
-    loadHome();
+  createHeader();
+  loadHome();
 }
 
 function createHeader() {
-    // Restaurant name
-    const header  = document.createElement('header');
-    const restaurantName = document.createElement('h1');
-    restaurantName.textContent = "Maria's Restaurant";
+  // Restaurant name
+  const header = document.createElement('header');
+  const restaurantName = document.createElement('h1');
+  restaurantName.textContent = "Maria's Restaurant";
 
-    // Home Menu Contact
-    const nav = document.createElement('nav');
-    const pageList = document.createElement('ul');
-    const home = document.createElement('li');
-    const menu = document.createElement('li');
-    const contact = document.createElement('li');
+  // Home Menu Contact
+  const nav = document.createElement('nav');
+  const pageList = document.createElement('ul');
+  const home = document.createElement('li');
+  const menu = document.createElement('li');
+  const contact = document.createElement('li');
 
-    home.textContent = 'Home';
-    menu.textContent = 'Menu';
-    contact.textContent = 'Contact';
+  home.textContent = 'Home';
+  menu.textContent = 'Menu';
+  contact.textContent = 'Contact';
 
-    pageList.append(home,menu,contact);
-    nav.appendChild(pageList);
+  pageList.append(home, menu, contact);
+  nav.appendChild(pageList);
 
-    // header.appendChild(restaurantName);
-    header.appendChild(nav);
+  // header.appendChild(restaurantName);
+  header.appendChild(nav);
 
-    contentDiv.appendChild(header);
+  contentDiv.appendChild(header);
 }
