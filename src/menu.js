4e08616af71b1a71dcf452af983d23b123daace7
Sameer <sameer4363@gmail.com>
2023-01-07 12:20:52 -0500

    Add menu creation code

--- src/menu.js
+++ src/menu.js
@@ -29,10 +29,52 @@ export default function loadMenu() {
     {
       name: 'Steak',
       cost: 21,
-      description: 'Juicy steak',
+      description: 'Sizzling steak',
+      category: 'entree',
+    },
+    {
+      name: 'Hamburger',
+      cost: 16,
+      description: 'Juicy burger',
       category: 'entree',
     },
   ];
 
-  console.log(menuItems[0].category);
+  // Sort menu items by category
+  menuItems.sort((a, b) =>
+    a.category > b.category ? 1 : b.category > a.category ? -1 : 0
+  );
+
+  const categories = [];
+  let currentCategory = menuItems[0].category;
+  let currentCategoryDiv = document.createElement('div');
+  let currentCategoryHeader = document.createElement('h2');
+
+  currentCategoryDiv.setAttribute('id', currentCategory);
+  currentCategoryHeader.textContent = currentCategory;
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItemDiv = document.createElement('div');
+    const menuItemName = document.createElement('h3');
+    const menuItemDescription = document.createElement('p');
+    const menuItemCost = document.createElement('p');
+
+    menuItemName.textContent = menuItems[i].name;
+    menuItemDescription.textContent = menuItems[i].description;
+    menuItemCost.textContent = menuItems[i].cost;
+
+    menuItemDiv.append(menuItemName, menuItemDescription, menuItemCost);
+
+    // create new category
+    if (menuItems[i].category !== currentCategory) {
+      contentDiv.append(currentCategoryHeader, currentCategoryDiv);
+      currentCategory = menuItems[i].category;
+      console.log(currentCategory);
+      currentCategoryDiv = document.createElement('div');
+      currentCategoryHeader = document.createElement('h2');
+      currentCategoryDiv.setAttribute('id', currentCategory);
+      currentCategoryHeader.textContent = currentCategory;
+    }
+    currentCategoryDiv.appendChild(menuItemDiv);
+  }
+  contentDiv.append(currentCategoryHeader, currentCategoryDiv);
 }
